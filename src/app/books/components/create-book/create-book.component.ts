e13dadfb0e8c46ec1d8ed4443de0e85c9daf3f2c
Alexei Pancratov <alexei.pancratov@endava.com>
2020-07-10 10:16:30 +0300

    Finished create-book form

--- src/app/books/components/create-book/create-book.component.ts
+++ src/app/books/components/create-book/create-book.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'create-book',
@@ -8,17 +12,76 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class CreateBookComponent {
   createBookForm: FormGroup;
+  requestErrorMessage: string = null;
+
+  constructor(private httpClient: HttpClient, private router: Router) {}
 
   ngOnInit() {
     this.createBookForm = new FormGroup({
-      author: new FormControl(null),
-      rentPrice: new FormControl(0),
-      salePrice: new FormControl(0),
-      title: new FormControl(null)
+      title: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(2)
+      ]),
+      author: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(4)
+      ]),
+      rentPrice: new FormControl(null, [
+        Validators.required
+      ]),
+      salePrice: new FormControl(null, [
+        Validators.required
+      ])
     });
   }
 
   onSubmit() {
-    console.log(this.createBookForm);
+    const rawValue = this.createBookForm.getRawValue();
+    const body = {
+      title: rawValue.title,
+      author: rawValue.author,
+      rentPrice: +rawValue.rentPrice,
+      salePrice: +rawValue.salePrice
+    };
+
+    this.httpClient.post('https://localhost:44378/books', body)
+      .pipe(
+        catchError(this.handleError)
+      )
+      .subscribe(() => this.router.navigate(['/books']));
+  }
+
+  get author() {
+    return this.createBookForm.get('author');
+  }
+
+  get rentPrice() {
+    return this.createBookForm.get('rentPrice');
+  }
+
+  get salePrice() {
+    return this.createBookForm.get('salePrice');
+  }
+
+  get title() {
+    return this.createBookForm.get('title');
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+      
+      this.requestErrorMessage = `${error.error.error}`;
+    }
+
+    return throwError(
+      'Something bad happened; please try again later.');
   }
 }
